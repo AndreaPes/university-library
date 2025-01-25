a9c9829e192298c1e8b990c03a1fd576a35e59eb
AndreaPes <10715428@polimi.it>
2025-01-25 23:25:39 +0100

    fix email

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "AndreaPes <hello.andreapes.pro>",
+      from: "AndreaPes <contact@andreapes.pro>",
       to: [email],
       subject,
       html: message,
